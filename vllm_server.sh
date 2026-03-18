731b1af7e81926bc605abc1acc484f878dc3c5bb
Abdullah Al Muhit <muhitmostakim09@gmail.com>
2026-03-18 16:42:00 -0400

    batch

---
+++ vllm_server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=vllm-server
+#SBATCH --account=muataz
+#SBATCH --partition=normal_q
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --time=72:00:00
+#SBATCH --output=/projects/meng/group23/logs/vllm_server_%j.log
+
+source /projects/meng/group23/envs/venv/bin/activate
+export HF_HOME=/projects/meng/group23/models/hf_cache
+
+vllm serve /common/data/models/mistralai--Mistral-Small-3.1-24B-Instruct-2503 --port 8000 --max-model-len 8192 --served-model-name mistral-small-24b --quantization fp8
